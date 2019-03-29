66700977584072fb4dfb7028751404fe81feb27f
Ferdinand Kramer <ferdinand.kramer@gmail.com>
2019-03-29 23:03:18 +0100

    feat(init): add h5bp

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,13 @@ export default {
 </script>
 
 <style>
+@import '../node_modules/html5-boilerplate/dist/css/normalize.css';
+
+@import './styles/h5bp/main.css';
+@import './styles/h5bp/base.css';
+@import './styles/h5bp/browserupgrade.css';
+@import './styles/h5bp/authors-custom.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +27,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+@import './styles/h5bp/helper.css';
+@import './styles/h5bp/example-mq-rwd.css';
+@import './styles/h5bp/print.css';
 </style>
